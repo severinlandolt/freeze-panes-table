7dcc0d065a70164845c88fb7064cfda3c25b9eab
Severin Landolt <sev.landolt@gmail.com>
2023-11-17 01:51:15 +0100

    update example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,233 @@
-import Image from 'next/image'
+const data = [
+  {
+    id: 1,
+    name: "Martha",
+    kilometers: 487000,
+    region: "Hamburg",
+    status: "overperforming",
+    deltaType: "moderateIncrease",
+    hours: 100,
+    productivity: "n/a",
+    specialSkill: "Exceptional Cooking",
+  },
+  {
+    id: 2,
+    name: "Captain Nemo",
+    kilometers: 32186,
+    region: "Nautilus",
+    status: "overperforming",
+    deltaType: "moderateIncrease",
+    hours: 110,
+    productivity: "n/a",
+    specialSkill: "Submarine Navigation",
+  },
+  {
+    id: 3,
+    name: "Professor Aronnax",
+    kilometers: 324000,
+    region: "Paris",
+    status: "underperforming",
+    deltaType: "moderateDecrease",
+    hours: 90,
+    productivity: "n/a",
+    specialSkill: "Marine Biology Expertise",
+  },
+  {
+    id: 4,
+    name: "Michael Strogoff",
+    kilometers: 200000,
+    region: "Russia",
+    status: "overperforming",
+    deltaType: "moderateDecrease",
+    hours: 92,
+    productivity: "n/a",
+    specialSkill: "Courageous Courier",
+  },
+  {
+    id: 5,
+    name: "Ned Land",
+    kilometers: 250000,
+    region: "Canada",
+    status: "underperforming",
+    deltaType: "moderateDecrease",
+    hours: 95,
+    productivity: "n/a",
+    specialSkill: "Expert Harpooner",
+  },
+  {
+    id: 6,
+    name: "Jean Passepartout",
+    kilometers: 275000,
+    region: "France",
+    status: "overperforming",
+    deltaType: "moderateIncrease",
+    hours: 98,
+    productivity: "n/a",
+    specialSkill: "Resourceful Valet",
+  },
+  {
+    id: 7,
+    name: "Gordon",
+    kilometers: 345000,
+    region: "New Zealand",
+    status: "underperforming",
+    deltaType: "moderateDecrease",
+    hours: 101,
+    productivity: "n/a",
+    specialSkill: "Leadership in Adversity",
+  },
+  {
+    id: 8,
+    name: "Axel Lidenbrock",
+    kilometers: 400000,
+    region: "Hamburg",
+    status: "overperforming",
+    deltaType: "moderateDecrease",
+    hours: 104,
+    productivity: "n/a",
+    specialSkill: "Geology Enthusiast",
+  },
+  {
+    id: 9,
+    name: "Otto Lidenbrock",
+    kilometers: 450000,
+    region: "Iceland",
+    status: "underperforming",
+    deltaType: "moderateIncrease",
+    hours: 107,
+    productivity: "n/a",
+    specialSkill: "Innovative Scientist",
+  },
+  {
+    id: 10,
+    name: "Helena Blunt",
+    kilometers: 480000,
+    region: "England",
+    status: "overperforming",
+    deltaType: "moderateDecrease",
+    hours: 110,
+    productivity: "n/a",
+    specialSkill: "Aristocratic Connections",
+  },
+  {
+    id: 11,
+    name: "Briant",
+    kilometers: 495000,
+    region: "Australia",
+    status: "underperforming",
+    deltaType: "moderateDecrease",
+    hours: 113,
+    productivity: "n/a",
+    specialSkill: "Survival Skills",
+  },
+  {
+    id: 12,
+    name: "Alice Strogoff",
+    kilometers: 310000,
+    region: "Russia",
+    status: "overperforming",
+    deltaType: "moderateIncrease",
+    hours: 116,
+    productivity: "n/a",
+    specialSkill: "Determination",
+  },
+  {
+    id: 13,
+    name: "Lady Helena",
+    kilometers: 360000,
+    region: "Scotland",
+    status: "underperforming",
+    deltaType: "moderateDecrease",
+    hours: 119,
+    productivity: "n/a",
+    specialSkill: "High Society Influence",
+  },
+];
+
+const footer = [
+  { prefix: "Count", value: 13 },
+  { prefix: "Sum", value: 4_408_186 },
+  { prefix: "", value: "" },
+  { prefix: "", value: "" },
+  { prefix: "Avg", value: 104.23 },
+  { prefix: "Avg", value: "---" },
+  { prefix: "", value: "" },
+];
+const header = [
+  "Name",
+  "Distance travelled (KM)",
+  "Region",
+  "Status",
+  "Working Hours (H)",
+  "Productivity",
+  "Special Skill",
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="resize overflow-auto border rounded">
+        <table className="border-separate border-spacing-0 w-full text-sm tabular-nums">
+          <thead className="h-10">
+            <tr className="font-medium">
+              {header.map((content, index) => (
+                <th
+                  key={index}
+                  className="first:pr-4 first:left-0 first:border-r first:z-20 top-0 bg-white whitespace-nowrap sticky border-b font-semibold text-right px-2.5"
+                >
+                  {content}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="text-right">
+            {data.map((item) => (
+              <tr key={item.id} className="whitespace-nowrap align-middle">
+                <td className="py-2 pr-4 align-middle sticky left-0 z-10 bg-white border-r px-2.5">
+                  {item.name}
+                </td>
+                <td className="py-2 px-2.5">
+                  {item.kilometers.toLocaleString()}
+                </td>
+                <td className="py-2 px-2.5">{item.region}</td>
+                <td className="py-2 px-2.5">
+                  <span
+                    className={`${
+                      item.deltaType === "moderateIncrease"
+                        ? " bg-emerald-50 text-emerald-700  ring-emerald-600/20"
+                        : "bg-rose-50 text-rose-700  ring-rose-600/20"
+                    } capitalize text-xs py-1 inline-flex items-center rounded-md font-medium ring-1 ring-inset px-2.5`}
+                  >
+                    {item.status}
+                  </span>
+                </td>
+                <td className="py-2 px-2.5">{item.hours}</td>
+                <td className="py-2 px-2.5 text-slate-400">
+                  {item.productivity}
+                </td>
+                <td className="py-2 px-2.5">{item.specialSkill}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot className="sticky bottom-0 bg-white z-10 font-medium border-t">
+            <tr className="text-right ">
+              {footer.map((item, index) => (
+                <td
+                  key={index}
+                  className="first:pr-4 first:bg-white first:sticky first:left-0 first:bottom-0 first:z-20 first:border-r py-2 align-middle border-t whitespace-nowrap px-2.5"
+                >
+                  {item.prefix ? (
+                    <span className="text-xs capitalize text-slate-400">
+                      {item.prefix}{" "}
+                    </span>
+                  ) : null}
+                  {item.value.toLocaleString()}
+                </td>
+              ))}
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </main>
-  )
+  );
 }
